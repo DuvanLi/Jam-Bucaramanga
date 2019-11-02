eb16d74326ed320f5b11c3312247f9d7c696dc97
Duvan Lizarazo <duvan12385@gmail.com>
2019-11-02 01:00:35 -0500

    Bullet system

---
+++ Assets/Duvan/Scripts/BulletController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    public float SleepTime;
+    public float activationRate;
+    public List<GameObject> bullets;
+
+    void Start()
+    {
+        StartCoroutine("Activate");
+    }
+
+    IEnumerator Activate()
+    {
+        for (int i = 0; i < bullets.Count; i++)
+        {
+            bullets[i].SetActive(true);
+            yield return new WaitForSeconds(activationRate);
+        }
+
+        yield return new WaitForSeconds(SleepTime);
+
+        for (int i = 0; i < bullets.Count; i++)
+        {
+            bullets[i].SetActive(false);
+            yield return new WaitForSeconds(activationRate);
+        }
+    }
+}
---
+++ Assets/Duvan/Scripts/WaypointSystem.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class WaypointSystem : MonoBehaviour
+{
+
+    public bool circuito;
+
+    public Color esferas;
+    public Color linea;
+
+    public List<Transform> waypoints = new List<Transform>();
+
+    int index = 0;
+
+    public bool disableInGame;
+
+    void Update()
+    {
+
+
+
+
+        if (!Application.isPlaying)
+        {
+            Transform[] tem = GetComponentsInChildren<Transform>();
+
+            if (tem.Length > 0)
+            {
+                waypoints.Clear();
+
+                index = 0;
+
+                foreach (Transform t in tem)
+                {
+                    if (t != transform)
+                    {
+
+
+                        t.name = "Way " + index.ToString();
+
+                        waypoints.Add(t);
+
+                        index++;
+                    }
+                }
+
+            }
+        }
+    }
+
+
+    void OnDrawGizmos()
+    {
+
+        if (waypoints.Count > 0)
+        {
+
+            Gizmos.color = esferas;
+
+            foreach (Transform t in waypoints)
+                Gizmos.DrawSphere(t.position, 1f);
+
+            Gizmos.color = linea;
+
+            for (int a = 0; a < waypoints.Count - 1; a++)
+                Gizmos.DrawLine(waypoints[a].position, waypoints[a + 1].position);
+
+            if (circuito)
+                Gizmos.DrawLine(waypoints[0].position, waypoints[waypoints.Count - 1].position);
+
+        }
+    }
+}
