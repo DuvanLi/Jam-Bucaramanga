b43aec0487327123e4b94478f3abf188978e62dd
hedersonleeh <hederson_3_12@hotmail.com>
2019-11-02 02:05:40 -0500

    Revert "Revert "Inventory System Firts steps""

---
+++ Assets/Code/Dialog.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Globalization;
+using UnityEngine;
+
+public class Dialog: MonoBehaviour
+{
+    public new string name;
+    [TextArea]public string text;
+    public Dialog nextDialog;
+    
+    [Space]
+   public Option[] option;
+}
