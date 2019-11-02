30b41634dcbd15d526bf1e4331ea51b318176768
Duvan Lizarazo <duvan12385@gmail.com>
2019-11-01 23:43:41 -0500

    Controller

---
+++ Assets/Duvan/Scripts/BulletMove1.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove1 : MonoBehaviour
+{
+
+    public float time;
+    
+    public float velocity;
+    public float amplitude;
+    public Rigidbody2D rb;
+
+    public bool invert;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Invoke("Kill", 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        time *= Mathf.Rad2Deg;
+
+        if (time >= 360)
+            time = 0;
+
+        time += Time.deltaTime * velocity;
+
+        time *= Mathf.Deg2Rad;
+
+        if (!invert)
+            rb.velocity = new Vector2(-velocity , Mathf.Sin(time) * amplitude);
+        else
+            rb.velocity = new Vector2(-velocity, -Mathf.Sin(time ) * amplitude);
+    }
+
+
+    private void Kill()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Duvan/Scripts/SmoothCamera2D.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothCamera2D : MonoBehaviour
+{
+
+    public float limitYUp;
+    public float limitYDown;
+
+    public float limitXRight;
+    public float limitXLeft;
+
+    public float dampTime = 0.15f;
+    private Vector3 velocity = Vector3.zero;
+    public Transform target;
+
+    public Camera camera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            Vector3 point = camera.WorldToViewportPoint(target.position);
+            Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+            Vector3 destination = transform.position + delta;
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, limitXLeft, limitXRight), Mathf.Clamp(transform.position.y, limitYDown, limitYUp), -10);
+        }
+
+    }
+}
