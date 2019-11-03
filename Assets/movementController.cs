4d6349c6adbab08a6f705302c7ac5b3d8591c75d
hedersonleeh <hederson_3_12@hotmail.com>
2019-11-02 19:10:11 -0500

    Revert "player now can jump fk yeah"
    This reverts commit 123f190bdfb324ab8c6901371a0e29086591eaa2.

--- Assets/movementController.cs
+++ Assets/movementController.cs
@@ -1,23 +1,17 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class movementController : MonoBehaviour
 {
     [SerializeField] private float jumpForce;
-    [SerializeField] private float speed;
+   [SerializeField]private float speed;
     private float horizontal;
     private bool facingRigh;
     private Animator animator;
     private Rigidbody2D rb;
-    private bool jump;
-    [SerializeField] private float offset;
-    [SerializeField] private LayerMask whatisGround;
-    [SerializeField] private Vector2 sizeBox;
-     private bool isjumping;
-
-    public void Start()
+    
+   public void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
@@ -27,8 +21,8 @@ public class movementController : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         print(horizontal);
+        
 
-        jump = Input.GetButtonDown("Button_A");
     }
 
     void FixedUpdate()
@@ -39,59 +33,42 @@ public class movementController : MonoBehaviour
 
         //rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x,targetVelocity,1f), rb.velocity.y);*/
 
-        if (rb.velocity.x >= 0.1 && facingRigh)
-        {
+        if(rb.velocity.x >= 0.1 && facingRigh)
+            {
             flip();
             facingRigh = !facingRigh;
         }
-
         if (rb.velocity.x <= -0.1 && !facingRigh)
         {
             flip();
             facingRigh = !facingRigh;
         }
-
-        switch (horizontal)
+            switch (horizontal)
         {
             case 0:
                 //rb.velocity = new Vector2((rb.velocity.x < 0) ? rb.velocity.x+Mathf.Abs(rb.velocity.x) :,rb.velocity.y)
                 rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, 0, 5), rb.velocity.y);
                 animator.SetBool("isRuning", false);
                 break;
-            case 1:
+            case 1:                
                 rb.velocity = new Vector2(speed, rb.velocity.y);
                 animator.SetBool("isRuning", true);
                 break;
             case -1:
-                rb.velocity = new Vector2(speed * -1, rb.velocity.y);
+                rb.velocity = new Vector2(speed*-1, rb.velocity.y);
                 animator.SetBool("isRuning", true);
                 break;
-        }
-
-
-        if (jump && !isjumping)
-        {
-            rb.AddForce(Vector2.up * jumpForce);
-            animator.SetTrigger("Jump");
-            isjumping = true;
-        }
+        }        
         
-    }
+       void flip()
+        {
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+       
+        }
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Ground"))
-            isjumping = false;
-    }
 
-    void flip()
-    {
-        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
+        
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.magenta;
-        Gizmos.DrawWireCube(new Vector2(transform.position.x, transform.position.y - offset), sizeBox);
-    }
+    
 }
